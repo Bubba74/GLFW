eb0a73e3e769a7241d9a6ded9ded6d1d9ac702b2
Henry Loh <pgloh.henry@gmail.com>
2018-10-12 11:19:43 -0700

    Added red strip to sphere

--- res/sphere.fs
+++ res/sphere.fs
@@ -2,7 +2,15 @@
 
 out vec4 FragColor;
 
+in float y;
 
 void main(){
-  FragColor = vec4(1, 1, 1, 1);
+//  float gray = (y+1)/2;
+
+  FragColor = vec4(0, 0, 1, 1);
+
+  float low = -0.3, high = -0.2;
+
+  if (low < y && y < high)
+    FragColor = vec4(1, 0, 0, 1);
 }
--- res/sphere.vs
+++ res/sphere.vs
@@ -7,8 +7,10 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 perspective;
 
+out float y;
 
 void main(){
+  y = pos.y;
   gl_Position = vec4(pos, 1);
   gl_Position = perspective * view * model * local * gl_Position;
 }
