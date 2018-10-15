2114b65a18fb364c67fe34394f1143b84e5829cb
Henry Loh <pgloh.henry@gmail.com>
2018-10-15 11:08:36 -0700

    Initial structs commit

---
+++ include/rubiks.h
@@ -0,0 +1,33 @@
+
+#ifndef RUBIKS_H
+#define RUBIKS_H
+
+#include <linmath.h>
+
+typedef struct rubiks_tile {
+
+  //rgb colors for the faces
+  vec3 faces[6];
+    //0 - top
+    //1 - right
+    //2 - front
+    //3 - back
+    //4 - left
+    //5 - bottom
+
+  mat4
+
+} tile;
+
+typedef struct rubiks_struct {
+
+
+
+
+
+
+} rubiks *;
+
+
+
+#endif RUBIKS_H
