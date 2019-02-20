f41d5d49b5dbf042aaa2189d12e915fe65757265
Henry Loh <pgloh.henry@gmail.com>
2019-02-20 00:40:08 -0800

    Cleaned up LTSS.fs

--- res/litTexturedSphereShader.fs
+++ res/litTexturedSphereShader.fs
@@ -12,15 +12,7 @@ void main(){
   vec4 dayColor = texture(day, TexCoord);
   vec4 nightColor = texture(night, TexCoord);
 
-  FragColor =
-  brightness * dayColor
-//  ;
-   +
-   0.5 * (1-brightness) * nightColor;
-//            + vec4((1-brightness)*nightColor.x,
-//                   (1-brightness)*nightColor.y,
-//                   (1-brightness)*nightColor.z,
-//                   1)
-                   ;
+  FragColor = brightness * dayColor + 0.5 * (1-brightness) * nightColor;
+
   FragColor.w = 1;
 }
